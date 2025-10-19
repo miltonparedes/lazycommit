e3a7cad7c80a261f8b508e7e3dca0d88036d61de
Milton Paredes <hey@milton.sh>
2025-10-18 20:37:03 -0600

    init go project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/miltonparedes/lazycommit
+
+go 1.25.3
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("lazycommit - AI-powered commit message generator")
+	fmt.Println("Initializing...")
+}
